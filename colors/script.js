9d8c9cd1c6cfbfc84b373a3bf7c700e479d6995c
kajlada <kajladaa@gmail.com>
2017-05-05 16:23:13 -0400

    Cleaned up code.
    Making it look a tad better.

--- colors/script.js
+++ colors/script.js
@@ -1,17 +1,13 @@
+
 function makeHex() {
     var text = "";
-    var letters = "abcdef0123456789"; //Hexcode needs letters between a and f && 0 - 9
-
-    for (var i = 0; i < 6; i++) //Hexcode length
-        text += letters.charAt(Math.floor(Math.random() * letters.length)); //Randomly picking a letter or number from letters
-
-    document.body.style.backgroundColor = "#" + text //generated hexcode
-    document.getElementById("hex").innerHTML = "#" + text; //changes h4 element of hexcode
-}
-
+    var letters = "abcdef0123456789"; //Letters and Numbers needed to create a hexcode (color).
+    for (var i = 0; i < 6; i++) //Length of #hexcode
+    text += letters.charAt(Math.floor(Math.random() * letters.length)); //Randomly picking a letter or number from letters
+    document.body.style.backgroundColor = document.getElementById("hex").innerHTML = "#" + text //Changing the background color & updating the h4
+}                                                                                               //which shows the new #hexcode
 
 document.body.onkeyup = function(e) {
-    if (e.keyCode == 32) {
-        makeHex();
-    }
+    if (e.keyCode !== 32) return; //If anything is clicked, nothing happens, but if space (keyCode: 32) is pressed the makeHex() function runs.
+    makeHex();
 }
