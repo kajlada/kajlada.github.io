046bcc2bc5098cab1db7fde39716a95a18fac35b
kajlada <kajladaa@gmail.com>
2017-05-05 15:05:37 -0400

    Revert "Shortened script.js"
    This reverts commit 454d2080bd2c4e02f5b7e96b72e2b21a84a7acf8.

--- colors/script.js
+++ colors/script.js
@@ -1,6 +1,17 @@
+function makeHex() {
+    var text = "";
+    var letters = "abcdef0123456789"; //Hexcode needs letters between a and f && 0 - 9
+
+    for (var i = 0; i < 6; i++) //Hexcode length
+        text += letters.charAt(Math.floor(Math.random() * letters.length)); //Randomly picking a letter or number from letters
+
+    document.body.style.backgroundColor = "#" + text //generated hexcode
+    document.getElementById("hex").innerHTML = "#" + text; //changes h4 element of hexcode
+}
+
+
 document.body.onkeyup = function(e) {
-    if (e.keyCode !== 32) return; //if space isn't pressed nothing happens.
-    document.body.style.backgroundColor = document.getElementById("hex").innerHTML = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-//randomly picked number between 0 - 1 which manipulates the 0xFFFFFF(a hexidecimal value) which represents rgb coordinates,
-//and then .toString(16) converts the hexadecimal value into text.
+    if (e.keyCode == 32) {
+        makeHex();
+    }
 }
